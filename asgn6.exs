cf30b340d54561dc81e1cc54c4e202dead72d05b
vanessalicia8 <vanessalicia8@gmail.com>
2019-03-05 22:10:56 -0800

    Add files via upload

--- asgn6.exs
+++ asgn6.exs
@@ -1,66 +1,106 @@
 #IO.puts "Hello world from elixir"
 
 defmodule NumC do
-	defstruct n: nil
+   defstruct n: nil
 end
 
 defmodule IdC do
-	defstruct sym: nil
+   defstruct sym: nil
 end
 
 defmodule StringC do
-	defstruct str: nil
+   defstruct str: nil
 end
 
 defmodule LamC do
-	defstruct params: nil, body: nil
+   defstruct params: nil, body: nil
 end
 
 defmodule IfC do
-	defstruct con: nil, den: nil, els: nil
+   defstruct con: nil, den: nil, els: nil
 end
 
 defmodule AppC do
-	defstruct fun: nil, args: nil
+   defstruct fun: nil, args: nil
 end
-
 
 
 
 
 defmodule Parser do
-	def parse(s) do
-		cond do
-			is_number(s) -> %NumC{n: s}
-			is_atom(s) -> %IdC{sym: s}
-			is_bitstring(s) -> %StringC{str: s}
-			is_tuple(s) ->
-				case s do
-					{:if, c1, c2, c3} ->
-						c_1 = parse(c1)
-						c_2 = parse(c2)
-						c_3 = parse(c3)
-						%IfC{con: c_1, den: c_2, els: c_3}
-					{:lam, args, body} ->
-						a = parse(args)
-						b = parse(body)
-						%LamC{params: a, body: b}
-					#{:var, 
-					_ -> "AppC, TODO"
-				end
-			true -> "ZHRL: invalid syntax"
-		end
-	end
+   def parse(s) do
+      cond do
+         is_number(s) -> %NumC{n: s}
+	 is_atom(s) -> %IdC{sym: s}
+	 is_bitstring(s) -> %StringC{str: s}
+	 is_tuple(s) ->
+        case s do
+           {:if, c1, c2, c3} ->
+		      c_1 = parse(c1)
+		      c_2 = parse(c2)
+		      c_3 = parse(c3)
+		      %IfC{con: c_1, den: c_2, els: c_3}
+	       {:lam, args, body} ->
+		      if is_list(args) do
+		         a = Enum.map(args, fn(x) -> parse(x) end)
+		         b = parse(body)
+		        %LamC{params: a, body: b}
+		     else "ZHRL: lam: invalid input"
+		     end
+	       _ ->
+		      cond do
+		         elem(s, 0) == :var ->
+			        if tuple_size(s) > 1 do
+                       parse(desugar(s))
+			        else "ZHRL: var: invalid input"
+			        end
+		        tuple_size(s) > 1 ->
+                   s1 = Tuple.to_list(s)
+			       arg_list = tl(s1)
+                   f = parse(hd(s1))
+			       if is_list(arg_list) do
+			          a = Enum.map(arg_list, fn(x) -> parse(x) end)
+			          %AppC{fun: f, args: a}
+			      else "ZHRL: app: invalid input"
+		          end
+		      end
+	       end
+	   true -> "ZHRL: invalid syntax"
+	 end
+   end
+
+   def desugar(s) do
+      size = tuple_size(s)
+      body = elem(s, size-1)
+      ss = (Tuple.to_list(s)) -- [:var, body]
+      params = Enum.map(ss, fn(x) -> get_params(x) end)
+      values = Enum.map(ss, fn(x) -> get_values(x) end)
+      l = [{:lam, params, body}] ++ values
+      List.to_tuple(l)
+   end
+   def get_params(e) do
+      case e do
+	 [sym, :=, _expr] -> sym
+	 _ -> "ZHRL: var: invalid input"
+      end
+   end
+   def get_values(e) do
+      case e do
+	 [_sym, :=, expr] -> expr
+	 _ -> "ZHRL: var: invalid input"
+      end
+   end
 end
 
-
+#TESTS
 defmodule Tests do
 	def main do
+		#PARSE
 		s = Parser.parse(5)
 		if s != %NumC{n: 5} do
 			IO.puts("fails")
 		end
-
+	
 		s = Parser.parse(:hi)
 		if s != %IdC{sym: :hi} do
 			IO.puts("fails")
@@ -76,7 +116,38 @@ defmodule Tests do
 			IO.puts("fails")
 		end
 
+		s = Parser.parse({:lam, [1], 2})
+		if s != %LamC{params: [%NumC{n: 1}], body: %NumC{n: 2}} do
+			IO.puts("fails")
+		end
 
+		s = Parser.parse({:f, 2})
+		if s != %AppC{fun: %IdC{sym: :f}, args: [%NumC{n: 2}]} do
+			IO.puts("app fails")
+		end
+
+		s = Parser.parse({:+, :y, 2})
+		if s != %AppC{fun: %IdC{sym: :+}, args: [%IdC{sym: :y}, %NumC{n: 2}]} do
+			IO.puts("app fails")
+		end
+        
+        s1 = Parser.desugar({:var, [:y, :=, 98], {:+, :y, 2}})
+        if s1 != {{:lam, [:y], {:+, :y, 2}}, 98} do
+            IO.puts("desugar fail")
+        end
+        
+		s = Parser.parse({:var, [:y, :=, 98], {:+, :y, 2}})
+        #IO.puts(s)
+		if s != %AppC{fun: %LamC{body: %AppC{args: [%IdC{sym: :y}, %NumC{n: 2}], 
+        fun: %IdC{sym: :+}}, params: [%IdC{sym: :y}]},
+        args: [%NumC{n: 98}]} do
+			IO.puts("var fails")
+		end
+
+		s = Parser.parse({:lam, 1, 2})
+		if s != "ZHRL: lam: invalid input" do
+			IO.puts("error case fail")
+		end
 
 
 	end
